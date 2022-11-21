356444f9fdcbdf37fb5e84d03e0805b19da843f6
zakogoto <nekitoss01@icloud.com>
2022-11-21 18:06:59 +0600

    almost finished slider

---
+++ dist/js/script.js
@@ -0,0 +1,9 @@
+const slider = tns({
+    container: '.slider',
+    mode: "gallery",
+    items: 3,
+    slideBy: 1,
+    arrowKeys: true,
+    preventActionWhenRunning: true,
+    center: true
+  });
