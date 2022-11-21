8193763b82b4351ab7d32af972e4a86ef6b5d7c4
zakogoto <nekitoss01@icloud.com>
2022-11-21 22:11:05 +0600

    start making map

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,19 @@
 const slider = tns({
     container: '.slider',
-    mode: "gallery",
-    items: 3,
+    items: 1,
     slideBy: 1,
-    arrowKeys: true,
+    autoplay: false,
+    fixedWidth: 600,
+    center: true,
+    controls: false,
+    nav: false,
     preventActionWhenRunning: true,
-    center: true
-  });
+    arrowKeys: true
+});
+
+document.querySelector('.prev').onclick = function() {
+    slider.goTo('prev');
+};
+document.querySelector('.next').onclick = function() {
+    slider.goTo('next');
+};
